6b8bc0f21b39be51d9802d32be46edb307f1350c
Marios <mariospatsis@outlook.com>
2018-11-14 14:15:02 +0200

    expensesSummary

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -9,8 +9,8 @@ const ExpenseListItem = ( {id,description,amount,createdAt}) =>(
        {numeral.locale('fr')}
        <Link to={'/edit/'+id}><h2>{description}</h2></Link>
        <p>
-       {numeral(amount/100).format('$ #,##0.00')} <br/>
-        - <br/>
+       {numeral(amount/100).format('$ #,##0.00')}
+        - 
         {moment(createdAt).format('MMMM Do, YYYY')}
         </p>
     </div>   
---
+++ src/components/ExpensesSummary.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import selectExpenses from '../selectors/expenses'
+import expensesTotal from '../selectors/expenses-total';
+import numeral from 'numeral';
+import 'numeral/locales/fr';
+
+
+
+export const ExpensesSummary = (props)=>(
+
+    <div>
+        <h2>Viewing {props.expenses.length} expenses totalling {numeral(props.total/100).format('$ #,##0.00')} </h2>
+    </div>
+
+);
+
+
+
+const mapStateToProps = (state) =>{
+    const expenses =  selectExpenses(state.expenses , state.filters);
+    return( 
+    {
+        expenses : expenses,
+        total: expensesTotal(expenses)
+    } 
+)
+} ;
+
+
+const connectedExpensesSummary = connect(mapStateToProps)(ExpensesSummary);
+
+export default connectedExpensesSummary;
