2aaa22bb74443d5b7b6ee7ff4f9e81cbd27cde76
Marios <mariospatsis@gmail.com>
2020-02-13 00:18:41 +0200

    env variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 
 
 const config = {
-    apiKey: "AIzaSyBJH2xC7CX5dI-P04Z5UQ7CBGaDrnujvhs",
-    authDomain: "expensify-app-e79d1.firebaseapp.com",
-    databaseURL: "https://expensify-app-e79d1.firebaseio.com",
-    projectId: "expensify-app-e79d1",
-    storageBucket: "expensify-app-e79d1.appspot.com",
-    messagingSenderId: "706609481258"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
   };
  
   firebase.initializeApp(config);
@@ -17,31 +17,3 @@ const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export { firebase,googleAuthProvider, database as default};
-
-// database.ref('expenses')
-//   .on('value',(snapshot)=>{
-    
-//     const expenses =[];
-
-//     snapshot.forEach(child => {
-       
-//         expenses.push({
-//             id: snapshot.key,
-//             ...snapshot.val()
-//         })
-
-//     });
-//     console.log(expenses);
-//   }); //end on 
-
-//   database.ref().set(
-//       {
-//       name: "Marios Patsis",
-//       age :26,
-//       isSingle:false,
-//       location :{
-//           city:"Athens",
-//           country:"Greece"
-//       }
-//   }
-//   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
+const fs = require('fs');
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env)=>{
     const isProduction = env == "production";
+
+    const currentPath = path.join(__dirname);
+    const basePath = currentPath + '/.env';
+    // Check if the .env exists
+    const fileEnv = fs.existsSync(basePath) ?
+        dotenv.config({ path: basePath }).parsed
+        :
+        process.env;
+    // Set the path parameter in the dotenv config
+    // const fileEnv = dotenv.config({ path: finalPath }).parsed;
+
+    const envKeys = Object.keys(fileEnv).reduce((prev, next) => {
+        prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
+        return prev;
+    }, {});
     
     return{
         entry : ['babel-polyfill','./src/app.js'],
@@ -35,7 +54,8 @@ module.exports = (env)=>{
         plugins:[
             new MiniCssExtractPlugin({
                 filename: "styles.css"
-            })
+            }),
+            new webpack.DefinePlugin(envKeys)
         ],    
         devtool: isProduction? 'source-map' : "inline-source-map",
     
